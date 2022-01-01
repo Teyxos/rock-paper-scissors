0d4a036f04a68e52996fa0d8bd3dd26d1985e5b3
Teyxos <teyxosesp@gmail.com>
2022-01-01 18:56:20 +0100

    Patch input problem

--- main.py
+++ main.py
@@ -1,43 +1,51 @@
 # TODO: While loop. Make the Rock Paper Scissors
 import random
 
-user_has_win = False
+answers = {
+    "Rock": 1,
+    "Paper": 2,
+    "Scissors": 3
+}
 
 
 def game():
-    user = get_choice().lower()
-    machine = random.choice(["Rock", "Paper", "Scissors"]).lower()
+    user = get_choice()
+    machine = answers[random.choice(["Rock", "Paper", "Scissors"])]
 
     winner = check_winner(user, machine)
 
     if winner == "user":
-        user_has_win = True
         print("Congratulations, you win!")
     elif winner == "machine":
         print("You lost :(")
+        game()
+    elif winner == "tie":
+        print("Tie!")
+        game()
 
 
 def get_choice():
     while 1:
         answer = input('Choose one "Rock", "Paper", "Scissors"\n')
-        print(answer)
-        if not answer:
-            continue
-        else:
+        answer = answers[answer]
+        if answer != 1 or answer != 2 or answer != 3:
+            print("False")
             break
+        else:
+            print("True")
+            continue
     return answer
 
 
 def check_winner(user, machine):
     if user == machine:
         return "tie"
-    elif user == "rock" and machine == "scissors" or user == "paper" and machine == "rock" or user == "scissors" and \
-            machine == "paper":
+    elif user == 1 and machine == 3 or user == 2 and machine == 1 or user == 3 and \
+            machine == 2:
         return "user"
-    elif user == "rock" and machine == "paper" or user == "scissors" and machine == "paper" or user == "paper" and \
-            machine == "scissors":
+    elif user == 1 and machine == 2 or user == 3 and machine == 2 or user == 2 and \
+            machine == 1:
         return "machine"
 
 
-while not user_has_win:
-    game()
+game()
