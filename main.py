20bd52fe5e8a6e7ab8bdd7eeb37a96b7a59fb431
Teyxos <teyxosesp@gmail.com>
2022-01-01 19:14:16 +0100

    Ended game

--- main.py
+++ main.py
@@ -25,25 +25,22 @@ def game():
 
 
 def get_choice():
-    while 1:
-        answer = input('Choose one "Rock", "Paper", "Scissors"\n')
+    answer = input('Choose one "Rock", "Paper", "Scissors"\n')
+
+    if answer == "Rock" or answer == "Paper" or answer == "Scissors":
         answer = answers[answer]
-        if answer != 1 or answer != 2 or answer != 3:
-            print("False")
-            break
-        else:
-            print("True")
-            continue
+    else:
+        answer = get_choice()
     return answer
 
 
 def check_winner(user, machine):
     if user == machine:
         return "tie"
-    elif user == 1 and machine == 3 or user == 2 and machine == 1 or user == 3 and \
-            machine == 2:
+    elif user == 1 and machine == 2 or user == 3 and machine == 1 or user == 2 and \
+            machine == 3:
         return "user"
-    elif user == 1 and machine == 2 or user == 3 and machine == 2 or user == 2 and \
+    elif user == 1 and machine == 3 or user == 3 and machine == 2 or user == 2 and \
             machine == 1:
         return "machine"
 
